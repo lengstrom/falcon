6572c6ab06a2cbad5f2a6ef377c7e8c81cb7dd01
lengstrom <lengstrom@users.noreply.github.com>
2016-07-02 15:56:29 -0700

    string matching bit works

--- emscripten/src/test.cpp
+++ emscripten/src/test.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
+#include <string>
+#include <assert.h>
 
-int ALPHABETLENGTH = 123;
+const int ALPHABETLENGTH = 123;
 
 extern "C" {
     void buildBMSTable(char * pat, int * memo) {
-        //printf("%s", pat);
-        // printf("%d", memo[0]);
-        std::cout << "New: " << pat << std::endl;
-        std::cout << memo[0] << std::endl;
-        memo[1] = 2;
+        int patLen = strlen(pat);
+        int i;
+        for (i = 0; i < ALPHABETLENGTH; i++) {
+            memo[i] = patLen;
+        }
+
+        for (i = 0; i < patLen - 1; i++) {
+            memo[pat[i]] = patLen - i - 1;
+        }
     }
 }
 
-/* extern "C" { */
-/*     int BMSMatch(char * pat, char * text, int * memo) { */
-/*         int patLen = strlen(pat); */
-/*         int textLen = strlen(text); */
-/*         int w = patLen - 1; */
-/*         int shift; */
+extern "C" {
+    int BMSMatch(char * pat, char * text, int * memo) {
+        int patLen = strlen(pat);
+        int textLen = strlen(text);
+        int w = patLen - 1;
+        int shift;
     
-/*         while (w < textLen) { */
-/*             shift = 0; */
-/*             while (text[w - shift] == pat[patLen - 1 - shift]) { */
-/*                 shift += 1; */
-/*                 if (patLen == shift) { */
-/*                     return 1; */
-/*                 } else { */
-/*                     break; */
-/*                 } */
-/*             } */
-/*             w += memo[w]; */
-/*         } */
+        while (w < textLen) {
+            shift = 0;
+            while (patLen > shift && text[w - shift] == pat[patLen - 1 - shift]) {
+                shift += 1;
+                if (patLen == shift) {
+                    return 1;
+                }
+            }
+            w += memo[text[w]];
+        }
 
-/*         return 0; */
-/*     } */
-/* } */
+        return 0;
+    }
+}
