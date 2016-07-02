82a7f7343faf0f592f68dabde95f734b8d9b2e57
lengstrom <lengstrom@users.noreply.github.com>
2016-07-02 01:22:49 -0700

    added emscriptem bs

--- emscripten/src/test.c
+++ emscripten/src/test.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-int strLenFn(char * str) {
-    return strlen(str);
+int ALPHABETLENGTH = 123;
+
+void buildBMSTable(char * pat, int * memo) {
+    memo[0] = 1;
+    int patLen = strlen(pat);
+    int i;
+    printf("%s", pat);
+    for (i = 0; i < ALPHABETLENGTH; i++) {
+        memo[i] = patLen;
+    }
+
+    for (i = 0; i < patLen - 1; i++) {
+        memo[pat[i]] = patLen - i - 1;
+    }
+}
+
+int BMSMatch(char * pat, char * text, int * memo) {
+    int patLen = strlen(pat);
+    int textLen = strlen(text);
+    int w = patLen - 1;
+    int shift;
+    
+    while (w < textLen) {
+        shift = 0;
+        while (text[w - shift] == pat[patLen - 1 - shift]) {
+            shift += 1;
+            if (patLen == shift) {
+                return 1;
+            } else {
+                break;
+            }
+        }
+        w += memo[w];
+    }
+
+    return 0;
 }
