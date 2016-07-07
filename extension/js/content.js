4d34befcaa7c77e64eb9acad842def9ff277e442
Andrew Ilyas <ailyas@mit.edu>
2016-07-06 20:31:35 -0400

    Add relevant text to chrome extension

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,11 +1,18 @@
 (function(){
     var epochTime = (new Date()).getTime();
     var url = window.location.href;
+    var nlparser = new NLParser()
+    var docString = document.documentElement.cloneNode(deep=true).outerHTML
+    var relText = document.body.innerText
+    if(!((window.location.protocol + "//" + window.location.host + "/") === window.location.href)) {
+        relText = nlparser.getRelevantText(docString)
+    }
+    console.log(relText)
     chrome.runtime.sendMessage({
         "msg":'pageContent',
         "time":epochTime,
         "url":url,
-        "text":document.body.innerText,
+        "text":relText,//document.body.innerText,
         "title":document.title,
     });
 })();
