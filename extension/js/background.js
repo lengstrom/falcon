b11f043f61274acb066c59fbead27947e2fa7de8
Andrew Ilyas <ailyas@mit.edu>
2016-09-10 00:59:55 -0400

    SAFE FROM BUGS

--- extension/js/background.js
+++ extension/js/background.js
@@ -15,19 +15,9 @@ Array.max = function( array ){
     return Math.max.apply(Math,array);
 };
 
-function ValidURL(str) {
-  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    '(\#[-a-z\d_]*)?$','i'); // fragment locater
-  if(!pattern.test(str)) {
-    alert("Please enter a valid URL.");
-    return false;
-  } else {
-    return true;
-  }
+function ValidURL(text) {
+    var valid = /((https?):\/\/)?(([w|W]{3}\.)+)?[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?/
+    return valid.test(text);
 }
 
 chrome.omnibox.onInputChanged.addListener(omnibarHandler);
